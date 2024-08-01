e28124a338cba9641423d77e7ff6b58b283cc0d4
developerkevo <developerkevo@gmail.com>
2024-08-01 11:36:42 +0300

    chore: Update signals.py to include PatientSettings model

--- core/signals.py
+++ core/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from core.models import CustomUser
 from donors.models import Donor, DonorSettings
-from patients.models import Patient
+from patients.models import Patient, PatientSettings
 from doctors.models import Doctor
 import uuid
 
